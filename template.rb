62596f86f49116c3f00efc38199dcb5ce8aa7989
amardaxini <amardaxini@gmail.com>
2010-09-12 01:52:16 +0530

    added email configuration support for gmail/sendgrd

--- template.rb
+++ template.rb
@@ -94,4 +94,26 @@ file "app/views/layouts/application.html.erb", <<-END
 </body>
 </html>
 END
+file "config/initializers/setup_mail.rb", <<-END 
+#ActionMailer::Base.smtp_settings = {
+#  :address              => "smtp.gmail.com",
+#  :port                 => 587,
+#  :domain               => "",
+#  :user_name            => "",
+#  :password             => "",
+#  :authentication       => "plain",
+#  :enable_starttls_auto => true
+#}
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :domain => "",
+  :authentication => :plain,
+  :user_name => "",
+  :password => "",
+  :enable_starttls_auto => true
+
+}
+ActionMailer::Base.default_url_options[:host] = "iitb.heroku.com"
+END
 
